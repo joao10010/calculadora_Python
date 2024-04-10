88f6f539eb4c207f519809cae47f9cf56c20d2a2
Joao <joao04silveira@gmail.com>
2024-04-09 22:16:07 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+num = int(input('digite um numero: '))
+num2 = int(input('digite segundo numero: '))
+
+print('* = multiplicação')
+print('+ = soma')
+print('- = menos')
+print('/ = divisao')
+
+funcao = (input('digite a funçao que voce quer fazer:'))
+
+if (funcao == '*'):
+    mul = num * num2
+    print(f'resultado {mul}')
+
+elif (funcao == '+'):
+    soma = num + num2
+    print(f'resultado {soma}')
+
+elif(funcao == '-'):
+    men = som - som1
+    print(f'resultado {men}')
+
+elif(funcao == '/'):
+    div = num /num2
+    print(f'resultado {div}')
+
+
+
+
+
+
+
+
+
+
